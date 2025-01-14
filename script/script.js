fcb730bfeb678046095ffca32d7d3455894b36ae
KaueGatti <kauegattti@gmail.com>
2025-01-14 14:12:06 -0300

    Refatorando id's e classes html e criando um novo script funcional para a interação com a galeria

--- script/script.js
+++ script/script.js
@@ -1,24 +1,39 @@
-const bNavEsquerda = document.getElementById("bNavEsquerda");
-const bNavDireita = document.getElementById("bNavDireita");
-const galeriaImagens = document.getElementById("galeriaImagensServico1");
-const larguraImg = document.querySelector("img").offsetWidth;
+const larguraImg = document.getElementsByTagName("img")[0].offsetWidth;
 const larguraImgRem = larguraImg / 16;
-const totalImagens = galeriaImagens.querySelectorAll("img").length;
-let indiceImg = 0;
-let moverImg = 0;
+const indiceImgs = {
+    "indice_galeriaImagensServico1": 0,
+    "indice_galeriaImagensServico2": 0,
+    "indice_galeriaImagensServico3": 0,
+    "indice_galeriaImagensServico4": 0,
+}
 
-bNavEsquerda.addEventListener("click", () => {
-  if (indiceImg > 0) {
-    --indiceImg;
-  }
-  moverImg = indiceImg * larguraImgRem;
-  galeriaImagens.style.transform = `translateX(-${moverImg}rem)`;
-});
+function moverEsquerda(id) {
+    const botaoSelecionado = id;
+    const galeriaImagens = document.getElementById(`${botaoSelecionado}`).nextElementSibling;
+    let idGaleria = galeriaImagens.id;
+    let indice = parseInt(indiceImgs[`indice_${idGaleria}`]);
+    indice = Math.max(0, Math.min(galeriaImagens.children.length - 1, indice));
+    if (indice <= 0) {
+        console.log("Primeira Imagem");
+    } else {
+        indice-= 1;
+        indiceImgs[`indice_${idGaleria}`] -= 1;
+        galeriaImagens.style.transform = `translateX(-${indice * larguraImgRem}rem)`;
+    }
+}
 
-bNavDireita.addEventListener("click", () => {
-  if (indiceImg < totalImagens - 1) {
-    ++indiceImg;
-  }
-  moverImg = indiceImg * larguraImgRem;
-  galeriaImagens.style.transform = `translateX(-${moverImg}rem)`;
-});
+function moverDireita(id) {
+    const botaoSelecionado = id;
+    const galeriaImagens = document.getElementById(`${botaoSelecionado}`).previousElementSibling;
+    let idGaleria = galeriaImagens.id;
+    let indice = parseInt(indiceImgs[`indice_${idGaleria}`]);
+    indice = Math.max(0, Math.min(galeriaImagens.children.length - 1, indice));
+    if (indice >= galeriaImagens.children.length - 1) {
+        console.log("Última Imagem");
+    } else {
+        indice+= 1;
+        indiceImgs[`indice_${idGaleria}`] += 1;
+        galeriaImagens.style.transform = `translateX(-${indice * larguraImgRem}rem)`;
+    }
+}
+
